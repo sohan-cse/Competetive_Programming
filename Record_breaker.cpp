740857d90e3ea4121d600f3c8eab9afc222b03e6
Sohan <sohan.cse.pust@gmail.com>
2024-11-04 16:39:18 +0600

    first commit

--- Record_breaker.cpp
+++ Record_breaker.cpp
@@ -1,2 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+void solve(vector<int>&arr,int n){
+    int mx=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(i==n-1&&arr[i]>mx){
+            cout<<arr[i]<<endl;
+        }
+        else if(arr[i]>arr[i+1] && arr[i]>mx){
+            cout<<arr[i]<<endl;
+            mx=arr[i];
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(auto&ele:arr){
+        cin>>ele;
+    }
+    solve(arr,n);
+}
